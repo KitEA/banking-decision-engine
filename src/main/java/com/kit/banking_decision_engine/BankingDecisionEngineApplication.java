1f0d47b725fcff1a4437579ad8f3f9f4406767a0
Kit_EA <kit.earf@gmail.com>
2026-01-07 16:35:24 +0400

    init commit

---
+++ src/main/java/com/kit/banking_decision_engine/BankingDecisionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.kit.banking_decision_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingDecisionEngineApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BankingDecisionEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kit/banking_decision_engine/BankingDecisionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kit.banking_decision_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDecisionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
